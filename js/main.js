a54254bbe361f2ae12d5969cdb5d188fc9086c49
Brian <Brianrhv@gmail.com>
2018-09-17 11:26:25 -0500

    Iniciando Proyecto

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let alumnosTodos = [
+    {
+        "codigo": "001",
+        "nombre": "Rodrigo",
+        "nota": "20"
+    },
+    {
+        "codigo": "002",
+        "nombre": "Miguel",
+        "nota"  : "11"
+    },
+    {
+        "codigo": "003",
+        "nombre": "Paul",
+        "nota": "14"
+    },
+    {
+        "codigo": "004",
+        "nombre": "Maria",
+        "nota": "09"
+    },
+    {
+        "codigo": "005",
+        "nombre": "Jose",
+        "nota": "16"
+    },
+    {
+        "codigo": "006",
+        "nombre": "Paula",
+        "nota": "15"
+    },
+    {
+        "codigo": "007",
+        "nombre": "Josephina",
+        "nota": "14"
+    },
+    {
+        "codigo": "008",
+        "nombre": "Mauricio",
+        "nota": "18"
+    }, {
+        "codigo": "009",
+        "nombre": "Daniela",
+        "nota": "17"
+    }, {
+        "codigo": "010",
+        "nombre": "Pepe",
+        "nota": "14"
+    }
+];
+
