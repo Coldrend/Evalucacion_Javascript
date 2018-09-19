c2d52856e95c6f7015970f5a05d30ad769a312d7
Brian <Brianrhv@gmail.com>
2018-09-18 19:56:57 -0500

    Javascript funcional

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ let alumnosTodos = [
     {
         "codigo": "001",
         "nombre": "Rodrigo",
-        "nota": "20"
+        "nota": "19"
     },
     {
         "codigo": "002",
         "nombre": "Miguel",
-        "nota"  : "11"
+        "nota": "11"
     },
     {
         "codigo": "003",
         "nombre": "Paul",
-        "nota": "14"
+        "nota": "09"
     },
     {
         "codigo": "004",
@@ -32,7 +32,7 @@ let alumnosTodos = [
     {
         "codigo": "007",
         "nombre": "Josephina",
-        "nota": "14"
+        "nota": "06"
     },
     {
         "codigo": "008",
@@ -41,11 +41,73 @@ let alumnosTodos = [
     }, {
         "codigo": "009",
         "nombre": "Daniela",
-        "nota": "17"
+        "nota": "07"
     }, {
         "codigo": "010",
         "nombre": "Pepe",
-        "nota": "14"
+        "nota": "05"
     }
 ];
 
+function alumnosLista() {
+    document.getElementsByClassName("contenido")[0].innerHTML = `
+            ${alumnosTodos.map(function (alumno) {
+            return `
+                <div class="alumnos">
+                <div class="marco">
+                    <img src="img/login/login-01.jpg">
+                </div>
+                <div class="alumno-info">
+                    <h3>${alumno.nombre} <span>${alumno.codigo}</span></h3>
+                    <h4>Nota: ${aprobado(alumno.nota)}</h4>
+                </div>
+                </div>
+                `
+        }
+        ).join('')}
+        `
+}
+function aprobado(nota) {
+    if (parseInt(nota) > 10) {
+        return `<span class="aprobado">${nota}</span>`
+    }
+    else {
+        return `<span class="desaprobado">${nota}</span>`
+    }
+}
+
+function mostrarTodo() {
+    alert(`${alumnosTodos.map(function (alumnoDatos) {
+        return alumnoDatos.nombre + " " +
+            alumnoDatos.codigo + " " +
+            alumnoDatos.nota + "\n" + "\n";
+    }
+    ).join('')}`);
+}
+
+function promedioAlumnos() {
+    let promedio=0;
+    let contador=0;   
+    alumnosTodos.map(function (alumno) {
+        promedio += (parseInt(alumno.nota));
+        contador++;
+    }
+    );
+    alert(`El promedio general es: ${promedio/contador}`);
+}
+
+function notaMayor(){
+    let notas = [];
+    alumnosTodos.map(function (alumno){
+        notas.push(parseInt(alumno.nota));
+    })
+    alert(`La mayor nota es: ${Math.max.apply(null, notas)}`)
+}
+
+function notaMenor() {
+    let notas = [];
+    alumnosTodos.map(function (alumno) {
+        notas.push(parseInt(alumno.nota));
+    })
+    alert(`La menor nota es: ${Math.min.apply(null, notas)}`)
+}
